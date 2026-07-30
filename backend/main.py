8ee1b38114b65282fbbaa5b38b7f4600da2ba0da
motorola <motorola12858@gmail.com>
2026-07-30 12:20:34 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
